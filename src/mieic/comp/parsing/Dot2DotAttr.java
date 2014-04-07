eaad7e5301df7cc676b341c3afae00a2560437a3
wso277 <oliveira.wilson93@gmail.com>
2014-04-07 16:29:28 +0100

    cenas

---
+++ src/mieic/comp/parsing/Dot2DotAttr.java
@@ -0,0 +1,16 @@
+public class Dot2DotAttr {
+	private String name;
+	/**
+	 * E - edges
+	 * N - nodes
+	 * G - graph
+	 * S - subgraph
+	 * C - cluster
+	 */
+	private Boolean usedBy[5];
+	private String type;
+	private String dflt;
+	private String minimum;
+
+	public Dot2DotAttr
+} 
