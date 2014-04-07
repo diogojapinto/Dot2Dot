bbe333911a49819136931b5bd39fa979786bda64
wso277 <oliveira.wilson93@gmail.com>
2014-04-07 16:46:33 +0100

    Added attr class

--- src/mieic/comp/parsing/Dot2DotAttr.java
+++ src/mieic/comp/parsing/Dot2DotAttr.java
@@ -1,16 +1,58 @@
 public class Dot2DotAttr {
 	private String name;
 	/**
-	 * E - edges
-	 * N - nodes
-	 * G - graph
-	 * S - subgraph
-	 * C - cluster
+	 * E - edges N - nodes G - graph S - subgraph C - cluster
 	 */
-	private Boolean usedBy[5];
+	private Boolean[] usedBy = null;
 	private String type;
 	private String dflt;
 	private String minimum;
 
-	public Dot2DotAttr
-} 
+	public Dot2DotAttr(Boolean[] u, String t, String d, String m) {
+		usedBy = u;
+		type = t;
+		dflt = d;
+		minimum = m;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Boolean[] getUsedBy() {
+		return usedBy;
+	}
+
+	public void setUsedBy(Boolean[] u) {
+		usedBy = u;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String t) {
+		type = t;
+	}
+
+	public String getDefault() {
+		return dflt;
+	}
+
+	public void setDefault(String d) {
+		dflt = d;
+	}
+
+	public String getMinimum() {
+		return minimum;
+	}
+
+	public void setMinimum(String m) {
+		minimum = m;
+	}
+
+}
